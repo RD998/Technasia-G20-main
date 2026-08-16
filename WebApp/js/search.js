b6ec8e3b50cb81245a9a289a0c883a51139e7b88
Rachit Dheri <89472661+RD998@users.noreply.github.com>
2026-08-16 22:19:34 +0530

    Add search functionality for app cards
    Implemented search functionality to filter app cards based on user input.

--- WebApp/js/search.js
+++ WebApp/js/search.js
@@ -1,4 +1,23 @@
+function searchWebsite() {
+    document.getElementById('recom').style.display = "none";
+    let filter = document.getElementById('search').value.toUpperCase();
 
+    let appcards = document.getElementById('appcards');
+    let card = appcards.getElementsByTagName('div');
+
+    for (var i = 0; i < card.length; i++) {
+        let h = card[i].getElementsByTagName('h3')[0];
+
+        let textValue = h.textContent || h.innerHTML;
+
+        if (textValue.toUpperCase().indexOf(filter) > -1) {
+            card[i].style.display = "block";
+        }
+        else {
+            card[i].style.display = "none";
+        }
+    }
+}
 
 if (document.getElementById('search').value.length === 0){
     document.getElementById('recom').style.display = "block";
